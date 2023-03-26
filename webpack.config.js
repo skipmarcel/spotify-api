55615f47e3d13b292b177c39fc28d165373fd8bd
Tyler Adams <skipmarcel@gmail.com>
2023-03-26 14:35:47 -0700

    finish project setup
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       verbose: true,
     }),
     new HtmlWebpackPlugin({
-      title: "Shape Tracker",
+      title: "Spotify API",
       template: "./src/index.html",
       inject: "body",
     }),
